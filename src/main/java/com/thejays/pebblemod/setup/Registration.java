e78e04e04cf049f1f35e2a800e36a39d6a6f63e4
Jason Harris <jasonharris306@gmail.com>
2022-07-15 13:37:47 +0100

    Cleaned up Registraion

--- src/main/java/com/thejays/pebblemod/setup/Registration.java
+++ src/main/java/com/thejays/pebblemod/setup/Registration.java
@@ -24,6 +24,7 @@ import java.util.stream.Collectors;
 public class Registration {
 
     //TODO: this needs to be done via some sort of config file
+    //      ideally i want a class that also has the biomes it should be generated in
     public static final String[] BLOCKS_TO_CONVERT = {
             "minecraft:stone",
             "minecraft:cobblestone",
@@ -31,14 +32,19 @@ public class Registration {
             "minecraft:sandstone",
             "minecraft:diorite",
             "minecraft:deepslate",
+            "minecraft:calcite",
     };
+
+    public static final Item.Properties ITEM_PROPERTIES = new Item.Properties().tab(ModSetup.ITEM_GROUP);
+    public static final Block.Properties PEBBLE_PROPERTIES = Block.Properties.of(Material.STONE)
+            .noOcclusion()
+            .strength(0.1f)
+            .noCollission()
+            .sound(SoundType.STONE);
+
 
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, UtilReference.MOD_ID);
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, UtilReference.MOD_ID);
-
-    //public static final RegistryObject<Block> COBBLESTONE_PEBBLE = BLOCKS.register("cobblestone_pebble", () -> new PebbleBlock(PEBBLE_PROPERTIES, ForgeRegistries.BLOCKS.getValue(new ResourceLocation("minecraft:cobblestone"))));
-    //public static final RegistryObject<Item> COBBLESTONE_PEBBLE_ITEM = fromBlock(COBBLESTONE_PEBBLE);
-
 
     public static void init() {
 
@@ -50,16 +56,13 @@ public class Registration {
         registerPebbles();
     }
 
-    public static final Item.Properties ITEM_PROPERTIES = new Item.Properties().tab(ModSetup.ITEM_GROUP);
-    public static final Block.Properties PEBBLE_PROPERTIES = Block.Properties.of(Material.STONE)
-            .noOcclusion()
-            .strength(0.1f)
-            .noCollission()
-            .sound(SoundType.STONE);
 
-
-    public static <B extends Block> RegistryObject<Item> fromBlock(@NotNull RegistryObject<B> block) {
+    public static <B extends Block> RegistryObject<Item> registerItemFromBlock(@NotNull RegistryObject<B> block) {
         return ITEMS.register(block.getId().getPath(), () -> new BlockItem(block.get(), ITEM_PROPERTIES));
+    }
+
+    public static RegistryObject<Block> registerPebbleBlock(String name, ResourceLocation resourceLocation){
+        return BLOCKS.register(name, () -> new PebbleBlock(PEBBLE_PROPERTIES, ForgeRegistries.BLOCKS.getValue(resourceLocation)));
     }
 
     public static void registerPebbles() {
@@ -67,17 +70,22 @@ public class Registration {
             var resourceLocation = new ResourceLocation(resourcePath);
             var pebbleName = resourcePath.substring(resourcePath.indexOf(':') + 1) + "_pebble";
 
-            var registryObjectBlock = BLOCKS.register(pebbleName, () -> new PebbleBlock(PEBBLE_PROPERTIES, ForgeRegistries.BLOCKS.getValue(resourceLocation)));
-            var registryObjectItem = fromBlock(registryObjectBlock);
+            var registryObjectBlock = registerPebbleBlock(pebbleName, resourceLocation);
+            var registryObjectItem = registerItemFromBlock(registryObjectBlock);
         }
     }
 
     public static List<RegistryObject<Block>> getPebbleBlocks() {
-        return BLOCKS.getEntries().stream().filter((blockRegistryObject) -> blockRegistryObject.get() instanceof PebbleBlock).collect(Collectors.toList());
+        return BLOCKS.getEntries()
+                .stream()
+                .filter((blockRegistryObject) -> blockRegistryObject.get() instanceof PebbleBlock)
+                .collect(Collectors.toList());
     }
 
     public static List<RegistryObject<Item>> getPebbleItems() {
-
-        return ITEMS.getEntries().stream().filter((itemRegistryObject) -> Block.byItem(itemRegistryObject.get().asItem()) instanceof PebbleBlock).collect(Collectors.toList());
+        return ITEMS.getEntries()
+                .stream()
+                .filter((itemRegistryObject) -> Block.byItem(itemRegistryObject.get().asItem()) instanceof PebbleBlock)
+                .collect(Collectors.toList());
     }
 }
