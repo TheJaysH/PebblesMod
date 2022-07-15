6c8b48899521bb7a8116f69f2505ff1ad9dcf08f
Jason Harris <jasonharris306@gmail.com>
2022-07-15 13:38:17 +0100

    Refactor Pebble generation code

--- src/main/java/com/thejays/pebblemod/datagen/PebbleBlockStates.java
+++ src/main/java/com/thejays/pebblemod/datagen/PebbleBlockStates.java
@@ -2,6 +2,7 @@ package com.thejays.pebblemod.datagen;
 
 import com.thejays.pebblemod.PebbleMod;
 import com.thejays.pebblemod.blocks.PebbleBlock;
+import com.thejays.pebblemod.helpers.PebbleHelper;
 import com.thejays.pebblemod.setup.Registration;
 import com.thejays.pebblemod.state.ModBlockStateProperties;
 import com.thejays.pebblemod.utils.UtilReference;
@@ -13,6 +14,8 @@ import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.RegistryObject;
 
 import java.util.List;
+
+import static net.minecraftforge.client.model.generators.ModelProvider.BLOCK_FOLDER;
 
 public class PebbleBlockStates extends BlockStateProvider {
 
@@ -36,28 +39,41 @@ public class PebbleBlockStates extends BlockStateProvider {
 
         List<RegistryObject<Block>> pebbles = Registration.getPebbleBlocks();
 
-        for (RegistryObject<Block> pebble: pebbles) {
+        for (RegistryObject<Block> pebble : pebbles) {
             registerPebble((PebbleBlock) pebble.get());
         }
 
     }
 
-    public void registerPebble(PebbleBlock pebbleBlock){
+    public void registerPebble(PebbleBlock pebbleBlock) {
         VariantBlockStateBuilder builder = this.getVariantBuilder(pebbleBlock);
         for (int i = 0; i < pebbleTemplates.length; i++) {
 
             String template = pebbleTemplates[i];
-            String suffix = i == 0 ? "" : "_" + (i + 1);
-            String path = UtilReference.MOD_ID + ":block/" + pebbleBlock.getRegistryName().getPath() + suffix;
+            String suffix = PebbleHelper.getSuffix(i);
+            String path = PebbleHelper.getResourcePath(
+                    UtilReference.MOD_ID, BLOCK_FOLDER,
+                    pebbleBlock.getRegistryName().getPath() + suffix
+            );
+            String templatePath = PebbleHelper.getResourcePath(
+                    UtilReference.MOD_ID,
+                    BLOCK_FOLDER,
+                    template
+            );
+            String parentPath = PebbleHelper.getResourcePath(
+                    BLOCK_FOLDER,
+                    pebbleBlock.parentBlock.getRegistryName().getPath()
+            );
+            ResourceLocation parentLocation = mcLoc(parentPath);
 
-            PebbleMod.LOGGER.info("Generating Model: " + path);
+            PebbleMod.LOGGER.info("Generating Block Model: " + path);
 
             BlockModelBuilder modelBuilder = models().getBuilder(path);
-            modelBuilder.texture("all", getResourceLocation(pebbleBlock.parentBlock));
-            modelBuilder.parent(getParent(new ResourceLocation(UtilReference.MOD_ID, "block/" + template)));
+            modelBuilder.texture("all", parentLocation);
+            modelBuilder.parent(getParent(PebbleHelper.getResourceLocation(templatePath)));
 
             VariantBlockStateBuilder.PartialBlockstate blockstate = builder.partialState()
-                    .with(ModBlockStateProperties.PEBBLES_1_4, Integer.valueOf(i + 1));
+                    .with(ModBlockStateProperties.PEBBLES_1_4, PebbleHelper.getInteger(i));
 
             ConfiguredModel[] configuredModel = ConfiguredModel.allYRotations(modelBuilder, 0, false);
 
@@ -66,7 +82,6 @@ public class PebbleBlockStates extends BlockStateProvider {
     }
 
     private ModelFile getParent(ResourceLocation resourceLocation) {
-        System.out.println("HELLO: " + resourceLocation);
         return new ModelFile(resourceLocation) {
             @Override
             protected boolean exists() {
@@ -75,7 +90,4 @@ public class PebbleBlockStates extends BlockStateProvider {
         };
     }
 
-    private ResourceLocation getResourceLocation(Block parentBlock) {
-        return new ResourceLocation("block/" + parentBlock.getRegistryName().getPath());
-    }
 }
--- src/main/java/com/thejays/pebblemod/datagen/PebbleItemModels.java
+++ src/main/java/com/thejays/pebblemod/datagen/PebbleItemModels.java
@@ -1,11 +1,15 @@
 package com.thejays.pebblemod.datagen;
 
 import com.thejays.pebblemod.PebbleMod;
+import com.thejays.pebblemod.helpers.PebbleHelper;
 import com.thejays.pebblemod.setup.Registration;
 import com.thejays.pebblemod.utils.UtilReference;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.client.model.generators.ModelBuilder;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 public class PebbleItemModels extends ItemModelProvider {
@@ -14,26 +18,26 @@ public class PebbleItemModels extends ItemModelProvider {
         super(generator, UtilReference.MOD_ID, fileHelper);
     }
 
-
     @Override
     protected void registerModels() {
 
         var pebbles = Registration.getPebbleItems();
         for (var pebble : pebbles) {
 
-            var item = pebble.get();
-            var itemPath = "item/" + item.getRegistryName().getPath();
+            Item item = pebble.get();
+            String itemPath = PebbleHelper.getResourcePath(ITEM_FOLDER, item.getRegistryName().getPath());
 
-            var block = Block.byItem(item);
-            var blockPath =  modLoc("block/" +  block.getRegistryName().getPath());
+            Block block = Block.byItem(item);
+            ResourceLocation blockPath = modLoc(PebbleHelper.getResourcePath(BLOCK_FOLDER, block.getRegistryName().getPath()));
 
             PebbleMod.LOGGER.info("Generating Item Model: " + itemPath);
-            PebbleMod.LOGGER.info("\tWith Block: " + blockPath);
 
-//            singleTexture(itemPath, mcLoc("item/generated"), "layer0", blockPath);
-
-            withExistingParent(itemPath, blockPath);
-//            withExistingParent(path, modLoc(Block.byItem(pebble.get().asItem()).getRegistryName().getPath()));
+            withExistingParent(itemPath, blockPath)
+                    .transforms()
+                    .transform(ModelBuilder.Perspective.GUI)
+                    .scale(2f)
+                    .rotation(20, 5, 10)
+                    .translation(0, 12, 5);
         }
 
     }
